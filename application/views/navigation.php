b1e82989fe8b8932b42ec77c4897d7e3200dcc98
Anaclet A <a.anaclet920@gmail.com>
2021-10-09 12:48:40 +0200

    navbar

--- application/views/navigation.php
+++ application/views/navigation.php
@@ -50,7 +50,7 @@
 
    <a href="<?= base_url() ?>" class="navbar-brand">
       <img src="<?= base_url() ?>assets/img/<?= webSettings()['logo'] ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light"><b><?= webSettings()['site_name'] ?></b></span>
+      <!-- <span class="brand-text font-weight-light"><b><?= webSettings()['site_name'] ?></b></span> -->
     </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
